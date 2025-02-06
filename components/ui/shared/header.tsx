d700daab93a26c53803a219afd190af19e444a07
kirillped <kirillpedev@gmail.com>
2025-02-06 16:24:37 +0300

    first commit

---
+++ components/ui/shared/header.tsx
@@ -0,0 +1,15 @@
+import { Container } from './container';
+import { cn } from '@/lib/utils';
+import * as React from 'react';
+interface Props {
+className?: string;
+}
+export const Header: React.FC<Props> = ({ className }) => {
+return (
+<header className={cn('border border-b', className)}>
+<Container className='flex items-center justify-between py-8'>
+    
+</Container>
+</header>
+);
+}
