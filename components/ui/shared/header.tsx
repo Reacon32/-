63ff8106e686ae08b7ad38838981b4afa1b95ac8
kirillped <kirillpedev@gmail.com>
2025-02-11 12:24:37 +0300

    sec

--- components/ui/shared/header.tsx
+++ components/ui/shared/header.tsx
@@ -1,6 +1,10 @@
 import { Container } from './container';
 import { cn } from '@/lib/utils';
+import  Image from 'next/image';
 import * as React from 'react';
+import { Button } from '../button';
+import { User } from 'lucide-react';
+import { ShoppingCart } from 'lucide-react';
 interface Props {
 className?: string;
 }
@@ -8,7 +12,28 @@ export const Header: React.FC<Props> = ({ className }) => {
 return (
 <header className={cn('border border-b', className)}>
 <Container className='flex items-center justify-between py-8'>
+<div className='flex'>
+    <div className='left flex items-center gap-4 '> 
     
+        <Image src='./pizza.svg' alt='logo' width={35} height={35} /> 
+    </div> 
+    <div> 
+        <h1 className='text-2x1 uppercase font-black'> NEXT PIZZA</h1> 
+        <p>Вкусней уже некуда</p> 
+    </div>
+</div>
+
+    <div className = 'flex items-center gap-4'>
+    <Button variant={"outline"} className = "gap-2">
+        <User size={16}/> Войти
+        </Button>
+
+       <Button variant={"outline"}>
+        <ShoppingCart size={16}/>
+     </Button> 
+    </div>
+
+
 </Container>
 </header>
 );
