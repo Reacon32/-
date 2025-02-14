8e86361151a530ef7428c39fa5c41f71986b96af
kirillped <kirillpedev@gmail.com>
2025-02-14 10:32:25 +0300

    sec

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { Container, Header } from "@/components/ui/shared";
+import { Title } from "@radix-ui/react-dialog";
 
 export default function Home() {
   return (
-    <h1>Главная</h1>
+    <h1>
+      
+    </h1>
   );
 }
+<Container className='flex items-center justify-between py-8'>
+<Title/>
+<h1 className='text-2x1 uppercase font-black'>Все Пиццы</h1>
+</Container>
--- components/ui/shared/index.ts
+++ components/ui/shared/index.ts
@@ -1,2 +1,5 @@
-export {Container} from "./container"
-export {Header} from "./header"
+export {Container} from './container';
+export {Categories} from './categories';
+export {Header} from './header';
+export {Title} from './title';
+export {SortPopup} from './sort-popup';
