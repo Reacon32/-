b7527bc88294023afc9ee5f470eb59f081ac7139
kirillped <kirillpedev@gmail.com>
2025-02-28 10:41:09 +0300

    https://github.com/Reacon32/-

---
+++ components/shared/filter.tsx
@@ -0,0 +1,17 @@
+import { Input } from "../ui/input";
+
+interface Props {
+    className?: string;
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+    return (
+        <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+            <p className="font-bold mp-3">Цена от и до</p>
+            <div className="flex gap-3 mb-5">
+                <Input type="number" placeholder="0" min={0} max={5000} defaultValue={0}/>
+                <Input type="number" min={100} max={5000} placeholder="5000"/>
+            </div>
+        </div>
+    );
+}
