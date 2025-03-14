bb24899718302975c6b50a30c3037ecd6b26f475
kirillped <kirillpedev@gmail.com>
2025-03-14 10:36:27 +0300

    update

--- Prisma/prisma-client.ts
+++ Prisma/prisma-client.ts
@@ -6,4 +6,5 @@ const prismaClientSingleton = () => {
         var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
         }
         export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
 if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
